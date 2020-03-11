aae41ec3ef9eb87061a389de9ec21f131d8f7f6d
Kashif Mahmood <31515792+intelligentCoding@users.noreply.github.com>
2020-03-11 13:16:38 -0400

    Added basic JavaScript

---
+++ script.js
@@ -0,0 +1,45 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+function updateSelectedCount(){
+	const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+
+	//copy selected seats into an arr
+	//we will use spread operator it copies the elments of an array
+	const seatsIndex = [...selectedSeats].map( (seat) => [...seats].indexOf(seat));
+	// console.log(seatsIndex);
+	// Now we need to store seatsIndex in local storage
+	localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+	//Map through array
+
+	// return new array
+
+
+
+	// console.log(selectedSeats);
+	// get the length of the node list
+	const selectedSeatCount = selectedSeats.length;
+	// console.log(selectedSeatCount);
+	count.innerText = selectedSeatCount;
+	total.innerText = selectedSeatCount * ticketPrice;
+}
+
+//this event gets triggered movie is selected
+movieSelect.addEventListener('change', (e) => {
+	ticketPrice  = +e.target.value;
+	updateSelectedCount();
+});
+
+//this eventlister gets called when you click on the seat
+container.addEventListener('click', (e) => {
+	if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied') ){
+		e.target.classList.toggle('selected');
+		updateSelectedCount();
+	}
+});
